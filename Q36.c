c52c38458d2316b73cd77a98d876de647fd25604
Arav-jain98 <aravjain778@gmail.com>
2025-09-15 23:18:52 +0530

    Add files via upload

---
+++ Q36.c
@@ -0,0 +1,14 @@
+//36.Write a program to find the HCF (GCD) of two numbers.
+#include <stdio.h>
+
+int main() {
+    int a,b,rem;
+    scanf("%d %d",&a , &b);
+    while(b !=0){
+        rem = b;
+        b = a%b;
+        a = rem;
+    }
+    printf("%d",a);
+    return 0;
+}
