7d828ca2c900cd617a432249c70b16b811af208e
Arav-jain98 <aravjain778@gmail.com>
2025-10-06 21:33:27 +0530

    Add files via upload

---
+++ Q57.c
@@ -0,0 +1,23 @@
+//Q57.Find the sum of array elements.
+
+#include <stdio.h>
+int main() {
+	int n,sum = 0 ;
+	printf("Enter size of array: ");
+	scanf("%d", &n);
+
+	int arr[n];
+	printf("Enter %d elements: ", n);
+	for(int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	printf("Sum of elements: ");
+	for(int i = 0; i < n; i++) {
+		sum += arr[i];
+
+    }
+    printf("%d ", sum);
+	printf("\n");
+	return 0;
+}
