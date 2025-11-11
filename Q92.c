a29bfcce2bcd4e443af65ea6d4077ac7be261d16
Arav-jain98 <aravjain778@gmail.com>
2025-11-11 21:36:39 +0530

    Add files via upload

---
+++ Q92.c
@@ -0,0 +1,29 @@
+//Q92.Find the first repeating lowercase alphabet in a string
+
+#include <stdio.h>
+
+int main() {
+    char str[] = "stress";
+    int count[26] = {0};
+    int i;
+    char firstRepeating = '\0';
+
+    for(i = 0; str[i] != '\0'; i++) {
+        count[str[i] - 'a']++;
+    }
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(count[str[i] - 'a'] > 1) {
+            firstRepeating = str[i];
+            break;
+        }
+    }
+
+    if(firstRepeating != '\0') {
+        printf("%c\n", firstRepeating);  
+    } else {
+        printf("No repeating character\n");
+    }
+
+    return 0;
+}
